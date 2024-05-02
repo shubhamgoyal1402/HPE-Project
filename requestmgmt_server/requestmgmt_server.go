3ace68a935ab49ce29eada4dc1cf4a5b51d22df2
shubham goyal <shubhamgoyal1402@gmail.com>
2024-05-02 17:40:28 +0530

    Create requestmgmt_server.go

---
+++ requestmgmt_server/requestmgmt_server.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/shubhamgoyal1402/hpe-golang-workflow/project/Queue"
+	pb "github.com/shubhamgoyal1402/hpe-golang-workflow/project/requestmgmt"
+	"google.golang.org/grpc"
+
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"time"
+
+	"os"
+	"os/exec"
+)
+
+var task_counter1 = 0
+var task_counter2 = 0
+var task_counter3 = 0
+var c = 0
+var a = 0
+var b = 0
+
+const cadenceCLIImage = "ubercadence/cli:master"
+const cadenceAddress = "host.docker.internal:7933"
+const domain = "day56-domain"
+const taskList = "Service_process"
+const workflowType = "github.com/shubhamgoyal1402/hpe-golang-workflow/project/worker/workflows.CustomerWorkflow"
+const (
+	port = ":50051"
+)
+
+type RequestManagementServer struct {
+	pb.UnimplementedRequestManagementServer
+}
+
+var Q1 = Queue.Queue{
+
+	Size: 10,
+}
+
+var Q2 = Queue.Queue{
+
+	Size: 10,
+}
+var Q3 = Queue.Queue{
+
+	Size: 10,
+}
+
+func (s *RequestManagementServer) CreateRequest(ctx context.Context, in *pb.NewRequest) (*pb.Request, error) {
+
+	log.Printf("received: %v", in.GetWid())
+	_, err := rpc1(ctx, in.GetWid(), in.GetRid(), in.GetId())
+
+	if err != nil {
+		log.Fatalf("error while enquing %v", err)
+	}
+	return &pb.Request{Wid: in.GetWid()}, nil
+
+}
+func rpc1(ctx context.Context, workflow_id string, rid string, id int32) (string, error) {
+	switch id {
+	case 1, 4:
+		ans, err := rpc_function1(ctx, workflow_id, id, rid, &Q1)
+		Q1.SortCustomers()
+		return ans, err
+	case 2, 5:
+		ans, err := rpc_function1(ctx, workflow_id, id, rid, &Q2)
+		Q2.SortCustomers()
+		return ans, err
+	case 3, 6:
+		ans, err := rpc_function1(ctx, workflow_id, id, rid, &Q3)
+		Q3.SortCustomers()
+		return ans, err
+	default:
+		return "Error Service not available ", errors.ErrUnsupported
+	}
+
+}
+func rpc_function1(ctx context.Context, workflow_id string, id int32, rid string, q *Queue.Queue) (string, error) {
+
+	for q.GetLength() >= q.Size/2 {
+		time.Sleep(time.Millisecond * 5)
+	}
+
+	customer1 := Queue.New(workflow_id, rid, ctx, id)
+	ans := fmt.Sprintf("Enqueud at %s", time.Now())
+	_, err := q.Enqueue(customer1)
+	if err != nil {
+		panic(err)
+	}
+	return ans, err
+}
+
+func main() {
+
+	go worker1()
+	go worker2()
+	go worker3()
+
+	log.Println("All workers ready ")
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("failed to listen : %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterRequestManagementServer(s, &RequestManagementServer{})
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve at: %v", err)
+	}
+
+	select {}
+}
+
+func worker1() {
+
+	for c > -1 {
+		if task_counter1 < 5 {
+			task_counter1++
+			go PrivateCloudEnterpriseServiceProcessing()
+			time.Sleep(time.Millisecond * 100)
+		} else {
+			time.Sleep(time.Second)
+		}
+
+	}
+}
+func worker2() {
+
+	for a > -1 {
+		if task_counter2 < 5 {
+
+			task_counter2++
+			go NetworkingServiceProcessing()
+			time.Sleep(time.Millisecond * 100)
+		} else {
+			time.Sleep(time.Second)
+		}
+
+	}
+
+}
+
+func worker3() {
+
+	for b > -1 {
+		if task_counter3 < 5 {
+			task_counter3++
+			go BlockStorageServiceProcessing()
+			time.Sleep(time.Millisecond * 100)
+		} else {
+			time.Sleep(time.Second)
+		}
+
+	}
+
+}
+
+func PrivateCloudEnterpriseServiceProcessing() {
+
+	response1, runid1, _, err1 := Q1.Dequeue()
+
+	if err1 == nil {
+
+		time.Sleep(time.Second * 10)
+
+		jsonSignal1, err := json.Marshal(response1)
+		if err != nil {
+			log.Fatalf("Error marshaling signal to JSON: %v", err)
+		}
+		signalcmd := fmt.Sprintf("docker run --rm %s --address %s -do %s workflow signal -w %s -r %s -n %s -i %s", cadenceCLIImage, cadenceAddress, domain, response1, runid1, response1, string(jsonSignal1))
+		go executeCommand(signalcmd)
+
+		time.Sleep(time.Millisecond)
+
+	}
+	task_counter1--
+}
+func NetworkingServiceProcessing() {
+	response2, runid2, _, err2 := Q2.Dequeue()
+
+	if err2 == nil {
+
+		time.Sleep(time.Second * 10)
+
+		jsonSignal2, err := json.Marshal(response2)
+		if err != nil {
+			log.Fatalf("Error marshaling signal to JSON: %v", err)
+		}
+		signalcmd := fmt.Sprintf("docker run --rm %s --address %s -do %s workflow signal -w %s -r %s -n %s -i %s", cadenceCLIImage, cadenceAddress, domain, response2, runid2, response2, string(jsonSignal2))
+
+		go executeCommand(signalcmd)
+
+		time.Sleep(time.Millisecond)
+
+	}
+	task_counter2--
+
+}
+
+func BlockStorageServiceProcessing() {
+
+	response3, runid3, _, err3 := Q3.Dequeue()
+
+	if err3 == nil {
+
+		time.Sleep(time.Second * 10)
+
+		jsonSignal3, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatalf("Error marshaling signal to JSON: %v", err)
+		}
+		signalcmd := fmt.Sprintf("docker run --rm %s --address %s -do %s workflow signal -w %s -r %s -n %s -i %s", cadenceCLIImage, cadenceAddress, domain, response3, runid3, response3, string(jsonSignal3))
+		go executeCommand(signalcmd)
+
+		time.Sleep(time.Millisecond)
+
+	}
+	task_counter3--
+}
+
+func executeCommand(command string) {
+	cmd := exec.Command("cmd", "/c", command)
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil {
+
+		os.Exit(1)
+	}
+
+}
