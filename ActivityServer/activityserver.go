15e739ac8528e8a5f28b17e815639f6f6bf9102a
shubham goyal <shubhamgoyal1402@gmail.com>
2024-05-25 01:41:19 +0530

    Create activityserver.go

---
+++ ActivityServer/activityserver.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func applicationDetails(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Application Details Submitted")
+
+}
+
+func validateUser(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "User Verified")
+
+}
+func getSubscription(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "Getting Subscription Details")
+
+}
+func Quiesce(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Quiesce Process started")
+
+}
+
+func storageSnapshot(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "Creating storage snapshot")
+
+}
+
+func unQuiesce(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "UnQuiesce Process started")
+}
+
+func backup(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "Catalogue Backup")
+
+}
+
+func blockStorage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "Block Storage granted")
+
+}
+
+func main() {
+	// Workflow 1
+
+	http.HandleFunc("/details", applicationDetails)
+	http.HandleFunc("/Quiesce", Quiesce)
+	http.HandleFunc("/snapshot", storageSnapshot)
+	http.HandleFunc("/UnQuiesce", unQuiesce)
+	http.HandleFunc("/backup", backup)
+
+	// Workflow 2
+
+	http.HandleFunc("/subscription", getSubscription)
+	http.HandleFunc("/validate", validateUser)
+	http.HandleFunc("/blockstorage", blockStorage)
+
+	fmt.Println("Starting server on :9090...")
+	if err := http.ListenAndServe(":9090", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+
+}
